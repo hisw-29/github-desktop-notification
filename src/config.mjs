cb3e625f852a007edd92a3540c1d1deca4787b49
hisw-29 <90458487+hisw-29@users.noreply.github.com>
2021-11-27 00:38:18 +0900

    fix title

--- src/config.mjs
+++ src/config.mjs
@@ -2,7 +2,7 @@ export const config = {
     "target_url": "https://github.com/notifications",
     "notification": {
         "type": "basic",
-        "title": "GitHub Notification",
+        "title": "GitHub Desktop Notification",
         "default_msg": "You got a notification.",
         "iconUrl": "/assets/images/icon128.png"
     }
